2e095a07671ff1cd85eff35ab89c31de31b75fac
Andrei Tiberiu Spanu <andreispanu@gmail.com>
2020-08-10 12:03:52 +0100

    Added Pins

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -2,11 +2,9 @@ import React, { useEffect } from 'react';
 
 const Map = (props) => {
 
-
-
   useEffect(() => {
 
-    const onScriptLoad = () => {
+    const initMap = () => {
       // Custom Silver Map
       var styledMapType = new window.google.maps.StyledMapType(
         require('./MapStyles.json')
@@ -14,7 +12,7 @@ const Map = (props) => {
       const map = new window.google.maps.Map(
         document.getElementById(props.id),
         {
-          center: { lat: 51.5074, lng: 0.1278 },
+          center: { lat: 51.5174, lng: 0.0278 },
           zoom: 11,
           disableDefaultUI: true,
           zoomControl: true
@@ -22,6 +20,64 @@ const Map = (props) => {
       );
       map.mapTypes.set('styled_map', styledMapType);
       map.setMapTypeId('styled_map');
+
+
+      var iconBase1 = require('../../static/images/pin1.png');
+      var iconBase2 = require('../../static/images/pin2.png');
+      var iconBase3 = require('../../static/images/pin3.png');
+      var iconBase4 = require('../../static/images/pin4.png');
+      var iconBase5 = require('../../static/images/pin5.png');
+
+      var icons = {
+        info1: {
+          icon: iconBase1
+        },
+        info2: {
+          icon: iconBase2
+        },
+        info3: {
+          icon: iconBase3
+        },
+        info4: {
+          icon: iconBase4
+        },
+        info5: {
+          icon: iconBase5
+        }
+      };
+
+      var features = [
+        {
+          position: new window.google.maps.LatLng(51.5574, 0.1279),
+          type: 'info1'
+        },
+        {
+          position: new window.google.maps.LatLng(51.5821, 0.1516),
+          type: 'info2'
+        },
+        {
+          position: new window.google.maps.LatLng(51.4121, 0.1216),
+          type: 'info3'
+        },
+        {
+          position: new window.google.maps.LatLng(51.5121, 0.2516),
+          type: 'info4'
+        },
+        {
+          position: new window.google.maps.LatLng(51.4121, 0.2216),
+          type: 'info5'
+        },
+      ]
+
+      // Create markers.
+      for (var i = 0; i < features.length; i++) {
+        var marker = new window.google.maps.Marker({
+          position: features[i].position,
+          icon: icons[features[i].type].icon,
+          map: map
+        });
+      };
+
     }
 
     if (!window.google) {
@@ -31,10 +87,10 @@ const Map = (props) => {
       var x = document.getElementsByTagName('script')[0];
       x.parentNode.insertBefore(s, x);
       s.addEventListener('load', e => {
-        onScriptLoad()
+        initMap()
       })
     } else {
-      onScriptLoad()
+      initMap()
     }
   }, [props.id])
 
